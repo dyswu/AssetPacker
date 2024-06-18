a11ddce46062e25774be59f62dd7210965b3c1f8
Derek Wu <54968178+dyswu@users.noreply.github.com>
2024-06-18 15:57:46 -0700

    GUI Experimentation

--- databases.py
+++ databases.py
@@ -37,11 +37,62 @@ package_columns = ['Package', 'Path', 'Name', 'Latest']
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 import pandas as pd
+import hashlib
 
 
+   
+fileName = ""
+file_df = pd.DataFrame(index = file_columns)
+tag_df = pd.DataFrame(index = tag_columns)
+package_df = pd.DataFrame(index = package_columns)
 
-fileName = ""    
+#Get hash value of file
+def md5(file_name):
+    m = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            m.update(chunk)
+        return m.hexdigest()
+    
+print("hello")
 
+#Add file to database
+def inputFile(path):
+    #TODO
+    print("Adding file to DB")
+    
+#Check to see if file already exists in database or not    
+def checkFile(file):
+    #TODO
+    print("check file") 
+
+#Create folders
+
+#Add file to DB
+def addFile():
+    print("select file to add")
+    path = askopenfilename()
+    file_name = path.split("/")[-1]
+    key = (md5(path))
+    #TODO Check existing file
+    #If yes -> file preview, ELSE
+    #TODO Ask for tags
+    #Add to existing project -> File name (new or updated)
+    #Get date
+    #If update -> get old id, set old next to key
+    #Save changes in db
+    saveData()
+
+#if load:
+#    file_df, tag_df, package_df, fileName = databases.loadData()
+    
+
+def resetData():
+    file_df = pd.DataFrame(Data = None, index = file_columns)
+    tag_df = pd.DataFrame(Data = None, index = tag_columns)
+    package_df = pd.DataFrame(Data = None, index = package_columns)
+    fileName = ""
+ 
 #Load file from existing file
 def loadData():
     #TODO Verify with test data
